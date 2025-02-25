99c82bd4594c4136ca665c621f43d6a2c6b80c2e
Priyul <Priyul20@gmail.com>
2025-02-25 10:19:46 +0200

    removed backend api calls

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -7,29 +7,30 @@ sap.ui.define(
         "use strict";
 
         return Controller.extend("mta.MTA1.controller.MainView", {
-            onInit: function () {},
-            onNavToSF: function() {
-                var oRouter = this.getOwnerComponent().getRouter();
-                oRouter.navTo("sf");
-            },
-            onNavToCurrUser: function() {
-                var oRouter = this.getOwnerComponent().getRouter();
-                oRouter.navTo("currUser");
-            },
-            onShowEmployees: function() {
-                var oTable = this.byId("employeeTable");
-                var oModel = this.getView().getModel("perPersonal");
+            onInit: function () {}
+            // ,
+            // onNavToSF: function() {
+            //     var oRouter = this.getOwnerComponent().getRouter();
+            //     oRouter.navTo("sf");
+            // },
+            // onNavToCurrUser: function() {
+            //     var oRouter = this.getOwnerComponent().getRouter();
+            //     oRouter.navTo("currUser");
+            // },
+            // onShowEmployees: function() {
+            //     var oTable = this.byId("employeeTable");
+            //     var oModel = this.getView().getModel("perPersonal");
                 
-                // Debug: Print the correct path data
-                console.log("Results array:", oModel.getProperty("/data/d/results"));
+            //     // Debug: Print the correct path data
+            //     console.log("Results array:", oModel.getProperty("/data/d/results"));
                 
-                // Make the table visible
-                oTable.setVisible(true);
+            //     // Make the table visible
+            //     oTable.setVisible(true);
                 
-                // Debug: Check table binding
-                console.log("Table binding path:", oTable.getBindingPath("items"));
-                console.log("Number of bound items:", oTable.getItems().length);
-             }
+            //     // Debug: Check table binding
+            //     console.log("Table binding path:", oTable.getBindingPath("items"));
+            //     console.log("Number of bound items:", oTable.getItems().length);
+            //  }
 
         });
     }
