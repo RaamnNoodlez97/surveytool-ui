6afc04357c7caf779ba894d3810fef3d055a6968
RaamnNoodlez97 <ashir.butt@epiuse.com>
2025-02-27 07:58:49 +0000

    updated routes

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -5,38 +5,27 @@ sap.ui.define(
      */
     function (Controller) {
         "use strict";
-
+ 
         return Controller.extend("mta.MTA1.controller.MainView", {
             onInit: function () {
-              var oModel = this.getView().getModel("sfData");
-              this.getView().setModel(oModel, "sfData");
-
-              console.log('in main view controller, sfData (principal propagation) oModel: ' + oModel);
-            }
-            // ,
-            // onNavToSF: function() {
-            //     var oRouter = this.getOwnerComponent().getRouter();
-            //     oRouter.navTo("sf");
-            // },
-            // onNavToCurrUser: function() {
-            //     var oRouter = this.getOwnerComponent().getRouter();
-            //     oRouter.navTo("currUser");
-            // },
-            // onShowEmployees: function() {
-            //     var oTable = this.byId("employeeTable");
-            //     var oModel = this.getView().getModel("perPersonal");
-                
-            //     // Debug: Print the correct path data
-            //     console.log("Results array:", oModel.getProperty("/data/d/results"));
-                
-            //     // Make the table visible
-            //     oTable.setVisible(true);
-                
-            //     // Debug: Check table binding
-            //     console.log("Table binding path:", oTable.getBindingPath("items"));
-            //     console.log("Number of bound items:", oTable.getItems().length);
-            //  }
-
+                var oModel = this.getView().getModel("sfData");
+                this.getView().setModel(oModel, "sfData");
+  
+                console.log('in main view controller, sfData (principal propagation) oModel: ' + oModel);
+              },
+              onShowHelloWorld: function() {
+                var oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("helloworld");
+              },
+              onShowSFPrincipalPropagation: function() {
+                var oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("sf");
+              },
+              onShowSFapi: function() {
+                var oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("sfapi");
+              }
+ 
         });
     }
 );
